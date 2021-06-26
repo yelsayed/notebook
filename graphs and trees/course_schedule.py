9539f3b4af0fc73e0a56fab8c2a12861bf73c2b9
Yasser El-Sayed <>
2021-06-26 20:52:00 +0300

    Added `top_sort` solution to the course schedule problem

--- graphs and trees/course_schedule.py
+++ graphs and trees/course_schedule.py
@@ -110,3 +110,83 @@ def can_finish_faster(numCourses: int, prerequisites: List[List[int]]) -> bool:
             return False
 
     return True
+
+
+"""
+The last way to do this, which is a lot faster but asymptotically the same is
+using Topological sort. I made a note of this in another file.
+
+Basically the idea is to keep track of all the nodes that have an in degree of
+0. Start iterating from there and removing edges from the graph. Whenever you
+remove an edge, you check if that node has now an in-degree of 0, if it does you 
+make sure to iterate over it again.
+
+That way the resulting graph should be empty, if it is, then there was a way to 
+topologically sort the graph. If not it means there was a cycle somewhere since
+the invariant was broken.
+"""
+
+class Node:
+    """ Special node to keep track of in-degrees """
+    def __init__(self):
+        self.out_nodes = []  # Out Nodes
+        self.in_deg = 0  # In Degrees
+
+
+def build_graph_with_in_degrees(prereqs):
+    """
+    This function will not just return the graph, but also the initial set
+    of nodes that have an in-degree of 0.
+    """
+    # We use our special Node to initialize the default dictionary
+    graph = defaultdict(Node)
+    degree_zero_nodes = set()
+
+    for a, b in prereqs:
+        # Same as before
+        graph[a].out_nodes.append(b)
+        graph[b].in_deg += 1
+
+        # Here we get the nodes with degree zero
+        if b in degree_zero_nodes:
+            degree_zero_nodes.remove(b)
+
+        # Here we check if the in degree of this node is 0 and add it to the set
+        if graph[a].in_deg == 0:
+            degree_zero_nodes.add(a)
+
+    return graph, degree_zero_nodes
+
+
+def can_finish(numCourses: int, prerequisites: List[List[int]]) -> bool:
+    graph, degree_zero_nodes = build_graph_with_in_degrees(prerequisites)
+
+    while len(degree_zero_nodes) > 0:
+        # Pop a random element :D
+        node = degree_zero_nodes.pop()
+        nodes = graph[node].out_nodes
+        to_be_removed = []
+        for ind, val in enumerate(nodes):
+            to_be_removed.append(ind)
+
+            # Remove the neighbouring node
+            neighbour = graph[val]
+            neighbour.in_deg -= 1
+
+            # If we know that this neighbour has no incoming nodes
+            # then we add it to the while iteration
+            if neighbour.in_deg == 0:
+                degree_zero_nodes.add(val)
+
+        # Remove them in the opposite order so as to not
+        # cause an index error when popping
+        for i in to_be_removed[::-1]:
+            nodes.pop(i)
+
+        # If it's empty and there are no more nodes,
+        # just remove it and we'll check `graph` later
+        if not nodes:
+            del graph[node]
+
+    # If all the edges are removed then we will
+    return not graph.keys()
